653248c206a048741fd22863071849c6f41719f7
R-romero <r.romero@outlook.com.br>
2019-03-18 23:26:18 -0300

    Skeleton Classes for HandsOn Component.

---
+++ begineers-guide/src/HandsOn/handsOn.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class HandsOn extends Component {
+  render() {
+    return (
+      0
+    );
+  }
+}
+
+export default HandsOn;
---
+++ begineers-guide/src/HandsOn/styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+import { device } from '../queries'
+
+export const codePen = styled.iframe `
+`
