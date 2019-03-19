8f85fa82a10123b5b6a32a653ca72b3fb62f4744
R-romero <r.romero@outlook.com.br>
2019-03-19 20:16:21 -0300

    Hands on Component Built and Styled.

--- begineers-guide/src/HandsOn/handsOn.js
+++ begineers-guide/src/HandsOn/handsOn.js
@@ -1,9 +1,59 @@
 import React, { Component } from "react";
+import {
+  Wrap,
+  Fiddle,
+  Title,
+  Header,
+  Nav,
+  Content,
+  Items,
+  Desc,
+  Scroll
+} from "./styled";
 
 class HandsOn extends Component {
+
+  state = {
+    iframeUrl: '//jsfiddle.net/Moltenbrain/9chnv6k3/1/embedded/html,result/dark/',
+  }
+
+  handleChangeIframe (url) {
+    this.setState({
+      iframeUrl: url
+    })
+  }
+
   render() {
     return (
-      0
+      <Wrap>
+        <Title>HandsOn</Title>
+        <Content>
+          <Header>
+            <Nav>
+              <Items onClick={() => this.handleChangeIframe("//jsfiddle.net/Moltenbrain/9chnv6k3/1/embedded/html,result/dark/")}>
+                <Desc>Create Element</Desc>
+              </Items>
+              <Items onClick={() => this.handleChangeIframe("//jsfiddle.net/Moltenbrain/vbenf3ar/embedded/html,result/dark/")}>
+                <Desc>Jsx Calls</Desc>
+              </Items>
+            </Nav>
+            <Scroll>
+              <Items>
+                <Desc> ↑ </Desc>
+              </Items>
+              <Items>
+                <Desc> ↓ </Desc>
+              </Items>
+            </Scroll>
+          </Header>
+          <Fiddle
+            src= {this.state.iframeUrl}
+            allowFullScreen="allowfullscreen"
+            frameBorder="0"
+            title="JsFiddle"
+          />
+        </Content>
+      </Wrap>
     );
   }
 }
--- begineers-guide/src/HandsOn/styled.js
+++ begineers-guide/src/HandsOn/styled.js
@@ -1,5 +1,82 @@
 import styled from 'styled-components'
 import { device } from '../queries'
 
-export const codePen = styled.iframe `
+export const Wrap = styled.div `
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: #b5c2b7;
+`
+
+export const Content = styled.div `
+  width: 90%;
+  align-self: center;
+`
+
+export const Title = styled.div `
+  font-family: 'Cambria';
+  padding-left: 5px;
+  font-size: 2em;
+  font-style: bold;
+  padding-bottom:10px;
+
+  @media ${device.tablet} {
+    padding-left: 15px;
+    font-size: 2.5em;
+  }
+
+  @media ${device.laptopL} {
+    padding-bottom: 20px;
+    font-size: 3em;
+  }
+`
+
+export const Header = styled.div`
+  display:flex;
+  background-color: #1c2128;
+  border: 2px solid #12151a;
+`
+
+export const Nav = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  display: grid;
+  text-align: center;
+  list-style: none;
+  grid-template-columns: repeat(2, 2fr);
+
+  /* @media ${device.laptop}{
+    grid-template-columns: repeat(4, 1fr);
+  } */
+`
+
+export const Items = styled.div `
+  display: flex;
+  flex-direction: column;
+  height: 50px;
+  color: #fff;
+  justify-content: center;
+  
+  &:hover {
+    background-color: #fff;
+    color: #000;
+  }
+  
+`
+
+export const Scroll = styled.div `
+  display: flex;
+  flex-direction: column;
+  height: 50px;
+`
+
+export const Desc = styled.div `
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+export const Fiddle = styled.iframe `
+  width: 100%;
+  height: 300px;
 `
