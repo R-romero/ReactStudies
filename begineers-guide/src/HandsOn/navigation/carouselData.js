cff5ca3f97d21e03e83d4fc5ce6be2ca31849a78
R-romero <r.romero@outlook.com.br>
2019-03-21 10:59:19 -0300

    CarouselData file

---
+++ begineers-guide/src/HandsOn/navigation/carouselData.js
@@ -0,0 +1,56 @@
+const CarouselData = () => {
+  return (
+    [
+      {
+        "name1": "Create Element",
+        "fiddle1": "//jsfiddle.net/Moltenbrain/9chnv6k3/embedded/html,result/dark/",
+        "name2": "Jsx Calls",
+        "fiddle2": "//jsfiddle.net/Moltenbrain/vbenf3ar/embedded/html,result/dark/"
+      },
+      {
+        "name1": "Custom Components",
+        "fiddle1": "//jsfiddle.net/Moltenbrain/pync15tq/embedded/html,result/dark/",
+        "name2": "Prop-types",
+        "fiddle2": "//jsfiddle.net/Moltenbrain/ndp2e4xj/embedded/html,result/dark/" 
+      },
+      {
+        "name1": "Conditional Rendering",
+        "fiddle1": "//jsfiddle.net/Moltenbrain/gva4h5pw/embedded/html,result/dark/",
+        "name2": "Re-rendering",
+        "fiddle2": "//jsfiddle.net/Moltenbrain/o9x7f2qn/embedded/html,result/dark/" 
+      },
+      {
+        "name1": "Styling Components",
+        "fiddle1": "//jsfiddle.net/Moltenbrain/1gL4530t/embedded/html,css,result/dark/",
+        "name2": "Event Handlers",
+        "fiddle2": "//jsfiddle.net/Moltenbrain/5x1mzy2h/embedded/html,result/dark/" 
+      },
+      {
+        "name1": "Component State",
+        "fiddle1": "//jsfiddle.net/Moltenbrain/2t5fpdg9/embedded/html,css,result/dark/",
+        "name2": "Class Component",
+        "fiddle2": "//jsfiddle.net/Moltenbrain/Lh8mv46q/3/embedded/html,result/dark/" 
+      },
+      {
+        "name1": "React References",
+        "fiddle1": "//jsfiddle.net/Moltenbrain/90tua5cw/embedded/html,css,result/dark/",
+        "name2": "Basic Forms",
+        "fiddle2": "//jsfiddle.net/Moltenbrain/a26tgnfw/embedded/html,result/dark/" 
+      },
+      {
+        "name1": "Dynamic Forms",
+        "fiddle1": "//jsfiddle.net/Moltenbrain/6u5djbnh/embedded/html,result/dark/",
+        "name2": "Form Values",
+        "fiddle2": "//jsfiddle.net/Moltenbrain/sd2axpu8/embedded/html,result/dark/" 
+      },
+      {
+        "name1": "Key Props",
+        "fiddle1": "//jsfiddle.net/Moltenbrain/37uLso06/embedded/html,result/dark/",
+        "name2": "HTTP Requests",
+        "fiddle2": "//jsfiddle.net/Moltenbrain/f3hmykt4/embedded/html,result/dark/" 
+      }
+    ]
+  )
+} 
+
+export default CarouselData;
